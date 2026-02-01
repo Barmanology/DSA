bd21a9899a102486d743cca67c55c47287c4ab16
Pradipta Barman <prbarman.career@gmail.com>
2026-02-02 00:08:27 +0530

    Lsa.java

--- Algos/Search/Linear/Lsa.java
+++ Algos/Search/Linear/Lsa.java
@@ -1,1 +1,30 @@
+public class Lsa
+{
+    public static int searchl(int lsr[], int N, int x)
+    {
+        for (int i = 0; i < N; i++)
+        {
+            if (lsr[i] == x)
+            return i + 1;
+        }
 
+
+        return -1;
+    }
+
+
+    public static void main(String Args[])
+    {
+        int arr[] = {1,2,3,4,5,6,7,8,23,324,23,56};
+        int target = 324;
+
+        int hit = searchl(arr, arr.length, target);
+
+        if (hit == -1)
+        System.out.println("No hit");
+        else
+        System.out.println("Hit at " + hit + "th place");
+
+
+    }
+}
