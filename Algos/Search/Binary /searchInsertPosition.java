8629dc2bfbc6a20460bfc70ea2c3b32558c66700
Pradipta Barman <prbarman.career@gmail.com>
2026-02-02 03:10:49 +0530

    SearchInsertPosition_01

---
+++ Algos/Search/Binary /searchInsertPosition.java
@@ -0,0 +1,23 @@
+class Solution {
+    public static int searchInsert(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        while (high >= low)
+        {
+            int mid = low + (high - low)/2;
+            if (nums[mid] == target)
+                return mid;
+            if (nums[mid] > target)
+                high = mid - 1;
+            else 
+                low = mid + 1;
+        }
+        return low;        
+    }
+
+    public static void main(String Args[])
+    {
+        int nums[] = {1,3,5,6}, target = 7;
+        int result = searchInsert(nums, target);
+        System.out.println(result);
+    }
+}
