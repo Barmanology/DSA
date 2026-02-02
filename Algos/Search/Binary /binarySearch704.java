4bdde42f4653df3db8adab83b29225aa961cd971
Pradipta Barman <prbarman.career@gmail.com>
2026-02-02 14:19:26 +0530

    Leetcode 704 Binary Search 02

--- Algos/Search/Binary /binarySearch704.java
+++ Algos/Search/Binary /binarySearch704.java
@@ -1,28 +1,62 @@
+/******WRONG ALGORITHM******/
+// class Solution {
+//     public static int search(int[] nums, int target) {
+        
+//         int low = 0, high = (nums.length - 1);
+
+//         while(high >= low)
+//         {
+//             int mid = low + (high - low)/2;
+
+//             if (nums[mid] == target)
+//                 return mid;
+//             if (nums[mid] > target)
+//                 high = mid - 1;
+//             else
+//                 low = mid + 1;
+
+//         }
+
+//         return -1;
+
+//     }
+
+//     public static void main(String Args[])
+//     {
+//         int nums[] = {-1,0,3,5,9,12};
+//         int target = 9;
+
+//         int result = search(nums, target);
+//         System.out.println(result);
+//     }
+// }
+
 class Solution {
     public static int search(int[] nums, int target) {
         
-        int low = 0, high = (nums.length - 1);
+        int low = 0, high = (nums.length - 1),ans = nums.length;
 
         while(high >= low)
         {
             int mid = low + (high - low)/2;
 
-            if (nums[mid] == target)
-                return mid;
-            if (nums[mid] > target)
+            if (nums[mid] >= target)
+            {
+                ans = mid;
                 high = mid - 1;
+            }
             else
                 low = mid + 1;
 
         }
 
-        return -1;
+        return ans;
 
     }
 
     public static void main(String Args[])
     {
-        int nums[] = {-1,0,3,5,9,12};
+        int nums[] = {-1,0,3,5,9,9,9,12};
         int target = 9;
 
         int result = search(nums, target);
